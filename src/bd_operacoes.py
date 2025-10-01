2697ddb5bd0b7278dee388a104e58ee395ed186f
gustavozequim <gustavozequim9@gmail.com>
2025-10-01 15:25:28 -0300

    Adiciona simples tratamento de erros

--- src/bd_operacoes.py
+++ src/bd_operacoes.py
@@ -1,19 +1,22 @@
 from pathlib import Path
 
 def HistoricoOperacoes(operacao):
-    print("Excelente, agora digite aqui o nome do arquivo com histórico de operações")
-    arq = str(input())
-    if not Path(f'{arq}.txt').exists():
-        print("Ops, vi que esse arquivo não existe, mas vamos criá-lo")
-        with open(f'{arq}.txt', 'w', encoding='utf-8') as arquivo:
-            arquivo.write(f'{operacao}\n')
-            arquivo.close()
-        print("Tudo certo!")
-        
-    else:
-        with open(f'{arq}.txt', 'a+', encoding='utf-8') as arquivo:
-            arquivo.write(f'{operacao}\n')
-            arquivo.close()
-        print("Arquivo atualizado com sucesso!")
-    
-    return arquivo    
+    try:
+        print("Excelente, agora digite aqui o nome do arquivo com histórico de operações")
+        arq = str(input())
+        if not Path(f'{arq}.txt').exists():
+            print("Ops, vi que esse arquivo não existe, mas vamos criá-lo")
+            with open(f'{arq}.txt', 'w', encoding='utf-8') as arquivo:
+                arquivo.write(f'{operacao}\n')
+                arquivo.close()
+            print("Tudo certo!")
+
+        else:
+            with open(f'{arq}.txt', 'a+', encoding='utf-8') as arquivo:
+                arquivo.write(f'{operacao}\n')
+                arquivo.close()
+            print("Arquivo atualizado com sucesso!")
+
+        return arquivo    
+    except Exception as e:
+        return print(f"Ocorreu um erro ao lidar com o arquivo de histórico:\n{e}")
--- src/raiz_quad.py
+++ src/raiz_quad.py
@@ -2,5 +2,8 @@ from datetime import datetime
 
 
 def RaizQuadrada():
-    a = int(input('Digite um numero: '))
-    return f"Raiz quadradada de {a} = {a ** 0.5} - {datetime.now().__format__('%d/%m/%Y - %H:%M:%S')}"
+    try:
+        a = int(input('Digite um numero: '))
+        return f"Raiz quadradada de {a} = {a ** 0.5} - {datetime.now().__format__('%d/%m/%Y - %H:%M:%S')}"
+    except Exception as e:
+        return print(f"Houve um erro ao calcular a raiz quadrada:\n{e}")
